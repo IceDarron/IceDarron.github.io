a61e8f71066ef687cb29a2d3e5bd42a8228a74ce
IceDarron <darron_r@163.com>
2018-06-19 01:05:40 +0800

    初始化，一个抖音中毒的项目。。。。。

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+$(function () {
+    $("#confirm").click(function () {
+        confirm("", "我喜欢你很久啦，你要不要做我女朋友啊~~~", function (isConfirm) {
+            if (isConfirm) {
+                love();
+            } else {
+                refuse();
+            }
+        }, {confirmButtonText: '确认', cancelButtonText: '关闭', width: 400, type: 'confession'});
+    });
 
+
+});
+
+
+function refuse() {
+    alert("", "你再考虑考虑啊，不要拒绝我嘛~~~", function () {
+        confirm("", "我喜欢你很久啦，你要不要做我女朋友啊~~~", function (isConfirm) {
+            if (isConfirm) {
+                love();
+            } else {
+                refuse();
+            }
+        }, {confirmButtonText: '确认', cancelButtonText: '关闭', width: 400, type: 'confession'});
+    }, {confirmButtonText: '确认', width: 400, type: 'confession'});
+}
+
+
+function love() {
+    alert("", "爱你爱你~~~", function () {
+    }, {confirmButtonText: '我也爱你~~~', width: 400, type: 'love'});
+}
